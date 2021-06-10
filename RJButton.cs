0516e82ade977f110e2ac2642b3229b4dc18bb61
RJCodeAdvance <52680606+RJCodeAdvance@users.noreply.github.com>
2021-06-10 14:08:14 -0500

    Add files via upload

--- RJButton.cs
+++ RJButton.cs
@@ -78,62 +78,65 @@ namespace CustomControls.RJControls
             this.Resize += new EventHandler(Button_Resize);
         }
 
-        //Methods
-        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+//Methods
+private GraphicsPath GetFigurePath(Rectangle rect, float radius)
+{
+    GraphicsPath path = new GraphicsPath();
+    float curveSize = radius * 2F;
+
+    path.StartFigure();
+    path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+    path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+    path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+    path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+    path.CloseFigure();
+    return path;
+}
+
+protected override void OnPaint(PaintEventArgs pevent)
+{
+    base.OnPaint(pevent);
+    pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+    Rectangle rectSurface = this.ClientRectangle;
+    Rectangle rectBorder = Rectangle.Inflate(rectSurface,-borderSize, -borderSize);
+    int smoothSize = 2;
+    if (borderSize > 0)
+        smoothSize = borderSize;
+
+    if (borderRadius > 2) //Rounded button
+    {
+        using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+        using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius-borderSize))
+        using (Pen penSurface = new Pen(this.Parent.BackColor, smoothSize))
+        using (Pen penBorder = new Pen(borderColor, borderSize))
         {
-            GraphicsPath path = new GraphicsPath();
-            path.StartFigure();
-            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
-            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
-            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
-            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
-            path.CloseFigure();
+            //Button surface
+            this.Region = new Region(pathSurface);
+            //Draw surface border for HD result
+            pevent.Graphics.DrawPath(penSurface, pathSurface);
 
-            return path;
+            //Button border                    
+            if (borderSize >= 1)
+                //Draw control border
+                pevent.Graphics.DrawPath(penBorder, pathBorder);
         }
-
-        protected override void OnPaint(PaintEventArgs pevent)
+    }
+    else //Normal button
+    {
+        //Button surface
+        this.Region = new Region(rectSurface);
+        //Button border
+        if (borderSize >= 1)
         {
-            base.OnPaint(pevent);
-            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-
-            RectangleF rectSurface = new RectangleF(0, 0, this.Width, this.Height);
-            RectangleF rectBorder = new RectangleF(1, 1, this.Width - 0.8F, this.Height - 1);
-
-            if (borderRadius > 2) //Rounded button
+            using (Pen penBorder = new Pen(borderColor, borderSize))
             {
-                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
-                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - 1F))
-                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
-                using (Pen penBorder = new Pen(borderColor, borderSize))
-                {
-                    penBorder.Alignment = PenAlignment.Inset;
-                    //Button surface
-                    this.Region = new Region(pathSurface);
-                    //Draw surface border for HD result
-                    pevent.Graphics.DrawPath(penSurface, pathSurface);
-
-                    //Button border                    
-                    if (borderSize >= 1)
-                        //Draw control border
-                        pevent.Graphics.DrawPath(penBorder, pathBorder);
-                }
-            }
-            else //Normal button
-            {
-                //Button surface
-                this.Region = new Region(rectSurface);
-                //Button border
-                if (borderSize >= 1)
-                {
-                    using (Pen penBorder = new Pen(borderColor, borderSize))
-                    {
-                        penBorder.Alignment = PenAlignment.Inset;
-                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
-                    }
-                }
+                penBorder.Alignment = PenAlignment.Inset;
+                pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
             }
         }
+    }
+}
         protected override void OnHandleCreated(EventArgs e)
         {
             base.OnHandleCreated(e);
